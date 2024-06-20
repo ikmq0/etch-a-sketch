f7bf4df2a4f45edf567124c36255f75e05c782ef
ikmq0 <71012568+ikmq0@users.noreply.github.com>
2024-06-20 14:16:38 +0300

    Change size optimization

--- script.js
+++ script.js
@@ -4,44 +4,45 @@ let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
-document.addEventListener("DOMContentLoaded", () => {
-  const gridSize = 32; // Change this value for a larger or smaller grid
-
+function createCells(gridSize) { 
   // Create grid cells
   for (let i = 0; i < gridSize * gridSize; i++) {
-      const cell = document.createElement("div");
-      cell.classList.add("cell");
-      cell.style.width = `${100 / gridSize}%`;
-      cell.style.height = `${100 / gridSize}%`;
-      cell.addEventListener('mouseover', changeColor)
-      cell.addEventListener('mousedown', changeColor)
-      container.appendChild(cell);
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.style.width = `${100 / gridSize}%`;
+    cell.style.height = `${100 / gridSize}%`;
+    cell.addEventListener('mouseover', changeColor)
+    cell.addEventListener('mousedown', changeColor)
+    container.appendChild(cell);
+  } 
+} 
+
+function removeCells() { 
+  for(const cell of container.querySelectorAll('.cell')) { 
+    container.removeChild(cell);
   }
+}
+document.addEventListener("DOMContentLoaded", () => {
+  const gridSize = 32; // Change this value for a larger or smaller grid
+  createCells(gridSize);
 });
 
 function changeColor(e) { 
   if(e.type === 'mouseover' & !mouseDown) return;
   e.target.style.backgroundColor = 'black';
+
 }
 
-
-// function InitializeSquares() {
-
-//   for(let i = 0; i < 1024; i++) { 
-//     const newSquare = document.createElement("div");
-//     newSquare.setAttribute("class","square");
-//     container.appendChild(newSquare)
-//     console.log("meow "+ i);
-//   }
-// }
 
 function ChangeSize(){
   const newSize = prompt("Enter the number of squares per side (100 is Maximum): ")
   if (newSize === null) {
     return; //break out of the function early
   }
-  while(!(newSize <= 100 && newSize >= 1)) { 
+  while(!(newSize < 100 && newSize >= 1)) { 
     newSize = prompt("Check your input!! \nEnter the number of squares per side (100 is Maximum): ")
   }
   // Complete Change Size Function
+  removeCells();
+  createCells(newSize);
 }
